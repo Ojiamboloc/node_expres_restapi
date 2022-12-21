77ff096a712e2037ebb0de1af0e3ee30013c0438
Ojiamboloc <ojiamboloc@gmail.com>
2022-12-21 06:43:25 -0800

    added dependecies

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import bodyParser from './bodyParser';
 const app=express();
 const port=5000;
 app.use(bodyParser.json());
+app.listen(port,()=>console.log(`Server listening on port:http://localhost:${port}`))
